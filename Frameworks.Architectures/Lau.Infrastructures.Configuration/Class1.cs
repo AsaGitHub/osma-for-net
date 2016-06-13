47a83d280aa1535de0fb68d26f4256f22e673bf7
Lau <security_accounts@163.com>
2016-06-13 19:25:15 +0800

    add empty solution
    add empty solution

---
+++ Frameworks.Architectures/Lau.Infrastructures.Configuration/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lau.Infrastructures.Configuration
+{
+    public class Class1
+    {
+    }
+}
---
+++ Frameworks.Architectures/Lau.UnitTest.TestCases/UnitTest1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Lau.UnitTest.TestCases
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            
+        }
+
+        public class asc { public string ast { get; set; } }
+    }
+}
